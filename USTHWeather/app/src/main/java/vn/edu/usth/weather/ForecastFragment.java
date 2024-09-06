954bbc9df21901d8b360a9ce2ddfc44d7b959d56
Thai Ha <hact.22bi13136@usth.edu.vn>
2024-09-06 13:31:06 +0700

    lab4,5

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -1,20 +1,21 @@
 package vn.edu.usth.weather;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 public class ForecastFragment extends Fragment {
-
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_forecast, container, false);
-        view.setBackgroundColor(Color.parseColor("#20FF0000"));
-
         return view;
     }
 }
