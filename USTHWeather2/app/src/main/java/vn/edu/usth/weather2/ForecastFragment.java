1c5a8dcc264d692aaad127693795f202e3f9b0e8
Thai Ha <hact.22bi13136@usth.edu.vn>
2024-09-17 13:33:52 +0700

    Lab4

--- USTHWeather2/app/src/main/java/vn/edu/usth/weather2/ForecastFragment.java
+++ USTHWeather2/app/src/main/java/vn/edu/usth/weather2/ForecastFragment.java
@@ -1,10 +1,16 @@
 package vn.edu.usth.weather2;
 
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 
 public class ForecastFragment extends Fragment {
 
@@ -12,8 +18,21 @@ public class ForecastFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_forecast, container, false);
-        view.setBackgroundColor( 0x20FF0000);
+        TextView day = new TextView(getContext());
+        day.setText("Thursday");
+
+        ImageView img = new ImageView(getContext());
+        img.setImageResource(R.drawable.sunny);
+
+        View v = new View(getContext());
+        v.setBackgroundColor(0x20FF0000);
+
+        LinearLayout view = new LinearLayout(getContext());
+        view.setOrientation(LinearLayout.VERTICAL);
+        view.addView(day);
+        view.addView(img);
+        view.addView(v);
+//        return inflater.inflate(R.layout.fragment_forecast, container, false);
         return view;
     }
 }
